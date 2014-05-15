c7d087cc422d8d09c4cb25eeffa895137c9322d1
Edward <bybeye@gmail.com>
2014-05-15 15:47:31 +0800

    test2

---
+++ Test2/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 2014-5-15
+ *      Author: edward
+ */
+
+#include "test.h"
+
+test::test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+test::~test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Test2/test.h
@@ -0,0 +1,17 @@
+/*
+ * test.h
+ *
+ *  Created on: 2014-5-15
+ *      Author: edward
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+#endif /* TEST_H_ */
