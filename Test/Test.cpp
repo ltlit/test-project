bd1574f402d6e2001a4592badf9e1afa074916ff
Edward <bybeye@gmail.com>
2014-05-15 16:04:58 +0800

    1111

---
+++ Test/Test.cpp
@@ -0,0 +1,18 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: 2014-5-15
+ *      Author: edward
+ */
+
+#include "Test.h"
+
+Test::Test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Test::~Test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Test/Test.h
@@ -0,0 +1,17 @@
+/*
+ * Test.h
+ *
+ *  Created on: 2014-5-15
+ *      Author: edward
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class Test {
+public:
+	Test();
+	virtual ~Test();
+};
+
+#endif /* TEST_H_ */
